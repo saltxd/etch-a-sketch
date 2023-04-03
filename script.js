60f653deee7f26f2b701017665b23e80bd35c28c
marley <74526520+saltxd@users.noreply.github.com>
2023-04-03 10:53:47 -0600

    Started over. Back to square 1

--- script.js
+++ script.js
@@ -1,42 +1,16 @@
-//script
-//const defaultSize = 16
 
-//let currentSize = defaultSize;
-
-//function createGrid(size){
-   // grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-   // grid.style.gridTemplateRows = `repeat(${size}, 1 fr)`
-
-   // for(let i = 0; i < size; i++) {
-   //     const gridElement = document.createElement('div')
-   //       gridElement.classList.add('grid-element')
-   //       gridElement.addEventListener('mouseover', changeColor)
-   //       gridElement.addEventListener('mousedown', changeColor)
-   //       grid.appendChild(gridElement)
-   // }
-//}
-
-//createGrid(defaultSize);
 
 const container = document.querySelector('.container');
 
-function createGrid(size) {
-  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  container.style.gridTemplateRows= `repeat(${size}, 1fr)`;
-
-  for (let i = 0; i < size * size; i++) {
+function createGrid(num) {
+  container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+  container.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+  for (let i = 0; i < num * num; i++) {
     const box = document.createElement('div');
-    box.classList.add('box');
-    containerElement.addEventListener('mouseover', changeColor)
-    containerElement.addEventListener('mousedown', changeColor)
+    box.classList.add('box-element');
     container.appendChild(box);
   }
 }
 
 createGrid(16);
-
-
-
-
-
 
