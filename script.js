54e15bc46001bfa10ae4bc1f52aa8b5df530a390
marley <74526520+saltxd@users.noreply.github.com>
2023-04-04 07:05:45 -0600

    Add eventlistener to change background on mouseover

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ function createGrid(num) {
   for (let i = 0; i < num * num; i++) {
     const box = document.createElement('div');
     box.classList.add('box-element');
+    box.addEventListener('mouseover', function(event){
+        event.target.style.backgroundColor = 'black';
+    })
+    //box.addEventListener('mouseover', changeColor)
+    //box.addEventListener('mousedown', changeColor)
     container.appendChild(box);
   }
 }
