aec2ca142d5c1cffc661be27a7421415179271cc
marley <74526520+saltxd@users.noreply.github.com>
2023-04-04 07:18:45 -0600

    Add reset button Add button with 'Reset' id to our html. Then added an eventlistener so when button is clicked it asks for # of squares per side. this then resets our createGrid function to the number of squares user inputs

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 
 
 const container = document.querySelector('.container');
+const resetButton = document.querySelector('#reset');
 
 function createGrid(num) {
   container.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
@@ -16,6 +17,15 @@ function createGrid(num) {
     container.appendChild(box);
   }
 }
+
+resetButton.addEventListener('click', () => {
+    let gridSize = prompt('Enter a number of squares per side (max 100):');
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    if (gridSize > 100) gridSize = 100;
+    createGrid(gridSize);
+});
 
 createGrid(16);
 
